c7cedac1b22afcddc24eff5e79ff3f99f8a2dd30
Htet Myat Hmue <htetmyathmue@gmail.com>
2024-10-14 01:11:42 +0630

    download, choose question types

--- utils/file_extractor.py
+++ utils/file_extractor.py
@@ -6,6 +6,8 @@ def extract_text_from_file(file):
         return extract_text_from_docx(file)
     elif file.filename.endswith('.pdf'):
         return extract_text_from_pdf(file)
+    elif file.filename.endswith('.txt'):
+        return extract_text_from_txt(file)
 
 def extract_text_from_docx(file):
     doc = docx.Document(file)
@@ -17,3 +19,6 @@ def extract_text_from_pdf(file):
     for page in reader.pages:
         text += page.extract_text()
     return text
+
+def extract_text_from_txt(file):
+    return file.read().decode('utf-8')  # Handle encoding if necessary
